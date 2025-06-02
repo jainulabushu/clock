968308fb132dc12eba0ad771ebae1718b3a6571b
Jainul <jainulabushu1@gmail.com>
2025-06-02 23:56:13 +0300

    analog

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const datediv = document.getElementById("datediv")
 const timediv = document.getElementById("timediv")
 const secStick = document.getElementById("secStick")
+const minStick = document.getElementById("minStick")
+const hourStick = document.getElementById("hourStick")
 
 
 
@@ -18,18 +20,42 @@ function updatetime(){
 }
 setInterval(updatetime, 1000);
 
-
-let secstickvalue = 0;
-
 function updatesec(){
     let now = new Date();
 
+    
     let hour = now.getHours();
     let min = now.getMinutes();
     let sec = now.getSeconds();
 
     secStick.style.rotate = `${sec*6}deg`;
+    minStick.style.rotate = `${min*6}deg`;
+    hourStick.style.rotate = `${hour*6}deg`;
+
 
 }
 setInterval(updatesec, 1000)
 
+// function updatemin(){
+    let now = new Date();
+
+    let fuck = 30;
+
+    let hour = now.getHours();
+    let min = now.getMinutes();
+    let sec = now.getSeconds();
+
+    minStick.style.rotate = `${fuck}deg`;
+
+
+// }
+// setInterval(updatemin, 60000)
+
+let x = 124;
+
+if (x < 13){
+    console.log("fuck you")
+}
+else(
+    console.log("fuck me")
+)
